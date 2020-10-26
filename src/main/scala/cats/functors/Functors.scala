0016daeaf5ad8359d4d53c22a0ea51ed170b94e4
Ravi <raviallishe@gmail.com>
2020-10-26 15:56:52 +0530

    adding syntactic sugar to Functor

--- src/main/scala/cats/functors/Functors.scala
+++ src/main/scala/cats/functors/Functors.scala
@@ -6,7 +6,7 @@ import cats.implicits.catsStdInstancesForList
 /**
   def map[A, B](fa: F[A])(f: A => B): F[B]
 
-  explanation map function in Functor.
+  explaining map function in Functor.
   (fa : F[A]) — type constructor: a data type that take some other data type e.g List, Future , Options
   (f: A => B ) — a function that transform a type A to type B
   F[B] — the final return type of the Functor's map function
@@ -18,7 +18,19 @@ object Functors extends App {
   //using List as functor
   val skills = List("scala", "akka", "kafka", "fp")
   val wordLength: String => Int = (a: String) => a.length
-  Functor[List].map(skills)(wordLength)
+  Functor[List]
+    .map(skills)(wordLength)
+    .foreach(println(_))
+
+//calling fproduct of Functor to give the element with value resulted after applying function on it.
+  Functor[List]
+    .fproduct(skills)(wordLength)
+    .foreach(println(_))
+
+  // after importing 'import cats.syntax.functor._' to allow user with syntactic sugar
+  import cats.syntax.functor._
+  skills
+    .fproduct(wordLength)
     .foreach(println(_))
 
 }
